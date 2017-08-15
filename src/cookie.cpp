b8309dccc353c0dfbfbe97625c1d89b3eec2e725
Terry Kelleher <tkelleher@newforcesolutions.com>
2017-08-15 09:52:01 -0400

    Fixes for clean install build

--- src/cookie.cpp
+++ src/cookie.cpp
@@ -18,7 +18,7 @@
 #  include <sys/time.h>
 #endif
  
-#include "../../cgicc-3.2.16/demo/styles.h"
+#include "/usr/share/doc/libcgicc-doc/examples/demo/styles.h"
 
 using namespace std;
 using namespace cgicc;
