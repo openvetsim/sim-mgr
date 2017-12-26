cf6802bd65e959dbcd4c09bfc37e92a5a38a8c13
davidwny <dweiner@twcny.rr.com>
2017-12-26 14:53:26 -0500

    Add files via upload

---
+++ bash1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install apache2
+sudo apt-get install mysql-server
+sudo apt-get install libapache2-mod-php7.0
+sudo apt-get apache2 restart
+
+php -r 'echo "\nPHP is running!! \n\n";'
+
+
+
+
