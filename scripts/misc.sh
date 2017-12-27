48dcec69e30ac4555e2f70dde3277b3a7eb92379
David Weiner <dweiner@twcny.rr.com>
2017-12-27 10:15:17 -0500

    Updated misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo 'this is a test'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
@@ -10,3 +9,6 @@ sudo apt-get install -y vsftpd
 sudo apt-get install -y openssh-server
 sudo apt-get install -y xdotool
 sudo apt-get install =y unclutter
+
+sudo cp vitals.sh /home/vitals/
+chown vitals:vitals /home/vitals/vitals.sh
