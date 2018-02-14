b961c77c6ab945f33b7d35ff160556e1a7309327
Terry K <terry@newforce.us>
2018-02-14 14:40:34 -0500

    Exit vitals.sh when chrome is killed

--- scripts/vitals.sh
+++ scripts/vitals.sh
@@ -15,8 +15,10 @@ export DISPLAY=:0
 unclutter &
  
 # If Chromium crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/vitals/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/vitals/.config/chromium/Default/Preferences
+if [ -f  /home/vitals/.config/chromium/Default/Preferences ]; then
+	sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/vitals/.config/chromium/Default/Preferences
+	sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/vitals/.config/chromium/Default/Preferences
+fi
 
 # get screen dimensions
 read RES_X RES_Y <<<$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}')
@@ -24,14 +26,16 @@ read RES_X RES_Y <<<$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}')
 # Run Chromium and open tabs
 #/usr/bin/chromium-browser --kiosk --window-position=0,0 --window-size="${RES_X},${RES_Y}" http://localhost/sim-ii/vitals.php &
 /usr/bin/google-chrome --kiosk --window-size="${RES_X},${RES_Y}" --incognito http://localhost/sim-ii/vitals.php &
+chromePid=$!
 
 # Start the kiosk loop. This keystroke changes the Chromium tab
 # To have just anti-idle, use this line instead:
 # xdotool keydown ctrl; xdotool keyup ctrl;
 # Otherwise, the ctrl+Tab is designed to switch tabs in Chrome
-# #
-while (true)
-  do
+# 
+# The "kill -0" checks that chrome is still running. The loop breaks if chrome is terminated
+##
+while kill -0 $chromePid 2> /dev/null; do
 	xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
 	sleep 15
 done
