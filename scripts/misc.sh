c7100a33f65b727dd80519131c10fe6f00136529
davidwny <dweiner@twcny.rr.com>
2018-11-01 09:16:00 -0400

    Update misc.sh
    Added sim-player clone, creation of video log directory and update permissions

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -17,3 +17,14 @@ sudo apt-get install -y obs-studio
 
 ./updateVitals.sh
 
+# clone sim-player
+cd /var/www/html/
+git clone https://github.com/tkelleher/sim-payer.git
+
+# create video directory for OBS
+mkdir /var/www/html/simlogs/video
+
+# update permissions
+chown www-data:www-data /var/wwww
+chmod 0755 /var/www/html
+chown -R www-data:www-data /vat/www/html
