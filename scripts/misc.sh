6326dd286702ecb959b6f8074b7e7bfa2b935581
davidwny <dweiner@twcny.rr.com>
2018-11-01 10:51:09 -0400

    Update misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -19,13 +19,13 @@ sudo apt-get install -y obs-studio
 
 # clone sim-player
 cd /var/www/html/
-git clone https://github.com/tkelleher/sim-payer.git
+git clone https://github.com/tkelleher/sim-player.git
 
 # create video directory for OBS
 mkdir /var/www/html/simlogs/video
 
 # update permissions
-chown www-data:www-data /var/wwww
+chown www-data:www-data /var/www
 chmod 0755 /var/www/html
 chown -R www-data:www-data /var/www/html
 chmod g+s /var/www/html/simlogs
