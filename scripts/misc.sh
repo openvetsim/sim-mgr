1a982a8d374175f05467127557b61cdf5c609632
Terry Kelleher <terry@newforce.us>
2018-11-01 20:01:46 +0000

    Install: Move clone of sim-player to follow ownership settings

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -17,10 +17,6 @@ sudo apt-get install -y obs-studio
 
 ./updateVitals.sh
 
-# clone sim-player
-cd /var/www/html/
-git clone https://github.com/tkelleher/sim-player.git
-
 # create video directory for OBS
 mkdir /var/www/html/simlogs/video
 
@@ -30,3 +26,7 @@ chmod 0755 /var/www/html
 chown -R www-data:www-data /var/www/html
 chmod g+s /var/www/html/simlogs
 chmod g+s /var/www/html/simlogs/video
+
+# clone sim-player
+cd /var/www/html/
+git clone https://github.com/tkelleher/sim-player.git
