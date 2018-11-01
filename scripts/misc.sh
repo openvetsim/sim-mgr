acf9c991534d59b2d35e667e489784e20e466c0c
davidwny <dweiner@twcny.rr.com>
2018-11-01 15:00:58 -0400

    Update misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -5,7 +5,7 @@ sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /et
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
 
-sudo apt-get install -y vsftpd
+# sudo apt-get install -y vsftpd
 sudo apt-get install -y openssh-server
 sudo apt-get install -y xdotool
 sudo apt-get install -y unclutter
