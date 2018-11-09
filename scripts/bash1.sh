44fbf5429b621c3d3d0da432d0a5864297db9442
davidwny <dweiner@twcny.rr.com>
2018-11-09 13:55:28 -0500

    Update bash1.sh

--- scripts/bash1.sh
+++ scripts/bash1.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get install -y apache2
 sudo apt-get install mysql-server
-sudo apt-get install -y libapache2-mod-php7.0
+#sudo apt-get install -y libapache2-mod-php7.0
+sudo apt-get install -y php
 sudo service apache2 restart
 
 php -r 'echo "\n PHP is RUNNING!!!\n\n";'
