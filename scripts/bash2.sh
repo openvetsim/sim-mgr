18169fc8890a3d6bfe9dd938055f24f24884b53f
davidwny <dweiner@twcny.rr.com>
2018-11-12 15:58:14 -0500

    Update bash2.sh

--- scripts/bash2.sh
+++ scripts/bash2.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y phpmyadmin apache2-utils
 if [[ ${RELEASE} == "18.04" ]]; 
 then
 	## echo Running 18.04.
-	sudo apt install php-dev libmcrypt-dev php-pear
+	sudo apt install -y php-dev libmcrypt-dev php-pear
 	sudo pecl channel-update pecl.php.net
 	sudo pecl install mcrypt-1.0.1
 else
@@ -20,8 +20,8 @@ sudo phpenmod mbstring
 sudo ln -s /usr/share/phpmyadmin /var/www/html
 sudo service apache2 restart
 
-if [[ ${RELEASE} == "18.04" ]]; 
-then
-	echo "You must edit /etc/php/7.2/cli/php.ini and /etc/php/7.2/apache2/php.ini"
-	echo "to add 'extension=mcrypt.so'"
-fi
+#if [[ ${RELEASE} == "18.04" ]]; 
+#then
+#	echo "You must edit /etc/php/7.2/cli/php.ini and /etc/php/7.2/apache2/php.ini"
+#	echo "to add 'extension=mcrypt.so'"
+#fi
