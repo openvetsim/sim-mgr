c4d0980a22edf49e2e59b09519d12ffcf56c7d6c
Terry Kelleher <tkelleher@newforcesolutions.com>
2021-01-21 14:06:05 -0500

    Permission changes for imnstall

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -18,55 +18,56 @@
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+sudo apt update
+sudo apt install -y google-chrome-stable
 
-# sudo apt-get install -y vsftpd
-sudo apt-get install -y openssh-server
-sudo apt-get install -y xdotool
-sudo apt-get install -y unclutter
-sudo apt-get install -y tinc
-sudo apt-get install -y net-tools
+# sudo apt install -y vsftpd
+sudo apt install -y openssh-server
+sudo apt install -y xdotool
+sudo apt install -y unclutter
+sudo apt install -y tinc
+sudo apt install -y net-tools
 
 sudo add-apt-repository -y ppa:obsproject/obs-studio
-sudo apt-get -y update
-sudo apt-get install -y obs-studio
+sudo apt -y update
+sudo apt install -y obs-studio
 
 ./updateVitals.sh
+
+# add user vet to www-data group
+sudo usermod -a -G www-data vet
+sudo usermod -a -G www-data simmgr
 
 # update permissions
 sudo chown www-data:www-data /var/www
 sudo chmod 0755 /var/www/html
 sudo chown -R www-data:www-data /var/www/html
 
-sudo mkdir /var/www/html/simlogs
-sudo chown www-data:www-data /var/www/html/simlogs
-sudo chmod 2777 /var/www/html/simlogs
-sudo chmod g+s /var/www/html/simlogs
+sudo -u www-data mkdir /var/www/html/simlogs
+sudo -u www-data chown www-data:www-data /var/www/html/simlogs
+sudo -u www-data chmod 2777 /var/www/html/simlogs
+sudo -u www-data chmod g+s /var/www/html/simlogs
 
 # create video directory for OBS
-sudo mkdir /var/www/html/simlogs/video
-sudo chmod g+sw /var/www/html/simlogs/video
+sudo -u www-data mkdir /var/www/html/simlogs/video
+sudo -u www-data chmod g+sw /var/www/html/simlogs/video
 
 # clone the sim-ii
 cd /var/www/html
-sudo git clone https://github.com/openvetsim/sim-ii.git
+sudo -u www-data git clone https://github.com/openvetsim/sim-ii.git
 
 cd /var/www/html
-sudo git clone https://github.com/openvetsim/scenarios.git
+sudo -u www-data git clone https://github.com/openvetsim/scenarios.git
 
 # clone sim-player
 cd /var/www/html/
-sudo git clone https://github.com/openvetsim/sim-player.git
+sudo -u www-data git clone https://github.com/openvetsim/sim-player.git
 
 # update ownership for all files....again
-sudo chown -R www-data:www-data /var/www/html
+#sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R g+w /var/www/html
 
 # update permissions for scenarios
 cd /var/www/html
 sudo chmod -R 777 ./scenarios
 
-# add user vet to www-data group
-sudo usermod -a -G www-data vet
-sudo usermod -a -G www-data simmgr
